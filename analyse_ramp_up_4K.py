2b42210dbfa3d7843e2beea06d32c3f665d9b4c5
Alexande Becker <becker.alexander42@gmail.com>
2021-05-12 16:12:02 +0200

    finished the ramp up analysis at 4K

--- analyse_ramp_up_4K.py
+++ analyse_ramp_up_4K.py
@@ -29,6 +29,8 @@ def gaussian(x, mu, sigma, A):
     """ A Gaussian function """
     return norm.pdf(x, loc=mu, scale=sigma)*A
 
+def fwhm_gauss(sigma):
+    return 2*np.sqrt(np.log(2)*2)*sigma
 # this part is the ramp selection part of the good ramp
 dbfn = "./ramp_up_ch2.dat"
 dufn = './ramp_up_ch1.dat'
@@ -56,11 +58,14 @@ dist_std = np.sqrt(plat_vals[1][1]**2 + plat_vals[0][1]**2)
 cfactor = 6/dist
 cfstd = cfactor*dist_std
 
+print("Calculated U -> B conversion factor: {}".format(cfactor))
+print()
+
 r = cb.where((cb['t'] > ramp_start_time) & (cb['t'] < ramp_end_time)).dropna()
-b = pd.Series(r['V']*cfactor, name='B')
-bstd = [np.sqrt((stdV/V)**2 + (cfstd/cfactor)**2) * mf for mf, stdV, V in zip(b, r['stdV'], r['V'])]
+bd = pd.Series(r['V']*cfactor, name='B')
+bstd = [np.sqrt((stdV/V)**2 + (cfstd/cfactor)**2) * mf for mf, stdV, V in zip(bd, r['stdV'], r['V'])]
 bstd = pd.Series(bstd, name='stdB', index=r.index)
-r = r.concat([r, b, bstd], axis=1)
+r = pd.concat([r, bd, bstd], axis=1)
 del r['V']
 del r['stdV']
 
@@ -74,73 +79,29 @@ popt, pcov = curve_fit(lin, r['t'], r['B'], sigma=r['stdB'])
 a = popt[0]
 b = popt[1]
 
-# load the dataset that we actually want to examine
-low_plateau_start = 583
-ramp_duration = 600
-
-slope = pd.read_csv("ramp_down_2K_B.dat", skiprows=2, delimiter=';')
-signal = pd.read_csv("ramp_down_2K_U.dat", skiprows=2, delimiter=';')
-slope = condense_data(slope)
-signal = condense_data(signal)
-low_plateau = cut_data(slope, 't',
-                       (low_plateau_start + 3, slope['t'].max()))
-down_ramp = cut_data(slope, 't',
-                     (slope['t'].min(), low_plateau_start - 3))
-lp_mean = low_plateau['V'].mean()
-lp_std = low_plateau['V'].std()
-
-rd_popt, rd_pcov = curve_fit(lin, down_ramp['t'], down_ramp['V'], p0=[-0.006, 4])
-print("Parameters for the downward slope:\na = {} +/- {}\nb = {} +/- {}".format(rd_popt[0],
-                                                                                rd_pcov[0][0],
-                                                                                rd_popt[1],
-                                                                                rd_pcov[1][1]))
-true_t0 = low_plateau_start - ramp_up_duration
-high_plateau = lin(true_t0, *rd_popt)
-high_plateau_std = np.sqrt(rd_pcov[1][1]**2 + (ramp_up_duration*rd_pcov[0][0])**2)
-rd_dist = high_plateau - lp_mean
-rd_dist_std = np.sqrt(high_plateau_std**2 + lp_std**2)
-rd_cf = 6 / rd_dist
-rd_cfstd = rd_dist_std * rd_cf
-print('Conversion factor for the ramp:\n{} +/- {}'.format(rd_cf, rd_cfstd))
-slope['V'] = slope['V'] - lp_mean
-slope['stdV'] = np.sqrt(slope['stdV']**2 + lp_std**2)
-
-
-b_down = pd.Series(slope['V'] * rd_cf, name='B', index=signal.index)
-b_down_std = pd.Series(np.sqrt((slope['stdV']/slope['V'])**2 + (rd_cfstd/rd_cf)**2) * b,
-                       name='stdB',
-                       index=signal.index)
-signal = pd.concat([signal, b_down, b_down_std], axis= 1)
-signal['V'] *= (-1)
-peak_borders = [(2.5, 4), (1.65, 2.15), (1.2, 1.5), (0.95, 1.125)]
-peak_params = []
-peak_covs = []
-figname = "uxx_ramp_down.pdf"
-signal.plot('B', 'V', label="Uxx Hall Voltage", linestyle='', marker='x', color='blue')
-plt.grid()
-plt.legend()
-plt.xlabel('Magnetic Field [T]')
-plt.ylabel('Hall Voltage [V]')
-plt.savefig(figname)
-plt.close()
-print("Plotted Uxx Hall Voltage to: {}".format(figname))
+print("fitted linear fnction f(x)=a*x+b to the channel 2 slope")
+print("the standard deviations are likely underestimated")
+print("a={} +/- {}".format(a, pcov[0][0]))
+print("b={} +/- {}".format(b, pcov[1][1]))
 print()
-print("Fitting Gaussian curves to the Uxx Peaks")
-print("----------------------------------------")
-for peak in peak_borders:
-    p = cut_data(signal, 'B', peak)
-    popt, pcov = curve_fit(gaussian, p['B'], p['V'])
-    print("Location of Peak: {:.5f} +/- {:.5f}".format(popt[0], np.sqrt(pcov[0][0])))
-    print("width of peak (sigma): {:.5f} +/- {:.5f}".format(popt[1], np.sqrt(pcov[1][1])))
-    p.plot('B', 'V', marker='x', color='blue', linestyle='', label='Uxx')
-    x = np.linspace(p['B'].min(), p['B'].max(), 1000)
-    plt.plot(x, gaussian(x, *popt), color='orange', linestyle='--', label='gaussian fit')
-    plt.legend()
+
+u = pd.concat([cd, bd, bstd], axis=1)
+print("Fitting the peaks of the Uxx voltage on the ramp up")
+print("---------------------------------------------------")
+peak_cuts = [(2.4, 4.5), (1.6, 2.2)]
+for cut in peak_cuts:
+    p = cut_data(u, 'B', cut)
+    popt, pcov = curve_fit(gaussian, p['B'], p['V'], sigma=p['stdV'])
+    print("for Peak between {} and {} T:".format(cut[0], cut[1]))
+    print("mean = ({} +/- {}) T".format(popt[0], np.sqrt(pcov[0][0])))
+    print("sigma = ({} +/- {}) T".format(popt[1], np.sqrt(pcov[1][1])))
+    print("FWHM = ({} +/- {}) T".format(fwhm_gauss(popt[1]), fwhm_gauss(np.sqrt(pcov[1][1]))))
+    p.plot('B', 'V', marker='x', linestyle='', color='blue', label='Uxx Hall voltage measurements')
+    plt.plot(p['B'], gaussian(p['B'], *popt), label='fitted gaussian', color='orange')
     plt.grid()
-    plt.xlabel('Magnetic Field Strength [T]')
-    plt.ylabel('Hall voltage [V]')
-    figname = 'uxx_peak_{}-{}T.pdf'.format(peak[0], peak[1])
+    plt.legend()
+    plt.xlabel('Magnetic Field Strength')
+    plt.ylabel('Uxx Hall voltage')
+    figname = 'uxx_4K_between_{}_and_{}T.pdf'.format(cut[0], cut[1])
     plt.savefig(figname)
     plt.close()
-    print("Saved plot of the fitted peak at: {}".format(figname))
-    print()
